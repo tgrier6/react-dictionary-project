fd5c00e38b0668c720fff1fe80b7e106f63bc125
tgrier6 <tiaragrier2@gmail.com>
2021-04-25 14:59:19 -0400

    Added logo and search form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Hello World!!!
+      <div className="container">
+        <header className="App-header">
+          <img
+            src="https://www.bostonmagazine.com/wp-content/uploads/sites/2/2018/03/dictionary.jpg"
+            className="App-logo"
+            alt="logo"
+          />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+      </div>
+      <footer className="text-center">
+        <small>
+          Coded by <a href="https://linkedin.com/in/tiaragrier2">Tiara Grier</a>{" "}
+          open-soucred on <a href="https://github.com/tgrier6">GitHub</a>.
+        </small>
+      </footer>
     </div>
   );
 }
